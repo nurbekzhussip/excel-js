064446475b48ab16ed6218defdd564945d54c5ac
Nurbek Zhussip <46653596+nurbekzhussip@users.noreply.github.com>
2020-10-01 20:14:53 +0600

    Framework start (#3)
    * Finish base framework functional
    
    * Test Websctorm commit

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,42 @@
+import {$} from '@core/DOM'
+
+export class Excel {
+    constructor(selector, option) {
+        this.$el = $(selector)
+        this.components = option.components || []
+    }
+
+    getRoot(){
+        const $root = $.create('div', 'excel')
+
+        this.components = this.components.map(Component => {
+
+            const $el = $.create('div', Component.className)
+
+            const component = new Component($el)
+            //DEBUG123
+            /*if(component.name) {
+                window['c'+component.name] = component
+            }*/
+            //$el.innerHTML = component.toHTML()
+
+            $el.html(component.toHTML())
+            $root.append($el)
+
+            return component
+        })
+        return $root;
+    }
+
+    render(){
+
+        this.$el.append(this.getRoot());
+
+        this.components.forEach(component => {
+            component.init()
+        })
+    }
+}
+
+
+
---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,22 @@
+import {DOMListener} from "@core/DOMListener";
+
+export class ExcelComponent extends DOMListener {
+
+    constructor($root, options = {}) {
+        super($root,options.listeners);
+        this.name = options.name || ''
+    }
+    //Return view components
+    toHTML() {
+        return ''
+    }
+
+    init(){
+        this.initDOMListeners()
+    }
+
+    destroy(){
+        this.removeDOMListeners()
+    }
+
+}
